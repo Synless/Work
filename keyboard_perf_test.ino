3836ebfd0382a4aad30770e3af359b3d3b8e752b
Synless <Synless@users.noreply.github.com>
2016-12-31 12:09:28 +0100

    Update keyboard_perf_test.ino

--- keyboard_perf_test.ino
+++ keyboard_perf_test.ino
@@ -1,39 +1,39 @@
 #include <Keyboard.h>;
 
+long t = 0;
+long counter = 0;
+bool test=false;
+
 void setup()
-{
-  SerialUSB.begin(115200);
-  pinMode(0,INPUT);
+{  
+  SerialUSB.begin();
   Keyboard.begin();
+  pinMode(0,INPUT);
 }
 
 void loop() 
-{
-  int counter = 0;
-  bool test = false;
-  t = micros();
-  while(micros()-t<1000000)
-  {
-    for(int n = 0; n < 30; n++)
-    {
-      if(test && digitalRead(0)==LOW)
-      {
-        test=false;
-      }
-      else if(!test && digitalRead(0)==HIGH);
-      {
-        test=true;
-      }
-      if(n%30==0)
-      {
-        Keyboard.release('a');  
-      }
-      
-    }
-    counter++;
+{  
+  if(micros()-t>1000000)
+  {    
+    SerialUSB.print("counter = ");  SerialUSB.println(counter);
+    counter=0;
+    delay(1000);    
+    t=micros();  
   }
-  SerialUSB.print("counter = ");  SerialUSB.println(counter);
-  delay(1000);
+  for(int n = 0; n < 30; n++)
+  {
+    if(test && digitalRead(0)==LOW)
+    {
+      test=false;
+    }
+    else if(!test && digitalRead(0)==HIGH);
+    {
+      test=true;
+    }
+    if(n%10==0)
+    {
+      Keyboard.release('a');  
+    }    
+  }
+  counter++;
 }
-
-//ABOUT 1000HZ IF "1/30 OF THE POLLS RAISE AN EVENT"
