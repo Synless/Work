c68084449583422b8fbd95d0502e46e408b0026d
Synless <Synless@users.noreply.github.com>
2016-12-31 18:09:40 +0100

    Add files via upload

---
+++ dma_adc_test/Joystick.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+class Joystick
+{
+  private:
+    int x;
+    int y;
+   
+  public:
+   Joystick(); 
+   int getX();
+   int getY();
+};
---
+++ dma_adc_test/dma_adc_test.ino
@@ -0,0 +1,20 @@
+#include "Joystick.h";
+
+Joystick joystick;
+
+void setup()
+{
+  SerialUSB.begin(115200);
+  while(!SerialUSB)
+  {
+    
+  }
+}
+
+void loop() 
+{
+  SerialUSB.println("Looping");
+  SerialUSB.print("A0 : ");SerialUSB.println(joystick.getX());
+  SerialUSB.print("A1 : ");SerialUSB.println(joystick.getY());
+  delay(50);
+}
